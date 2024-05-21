cef3c4399806695298918d0a74620429474111e6
@hexlet/cli <support@hexlet.io>
2024-05-21 13:30:43 +0300

    submit java-spring-ru/first-start

--- java-spring-ru/first-start/src/main/java/exercise/Application.java
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
     @GetMapping("/about")
