a478539afffd3871628d55fee163a2ac52c0fae2
@hexlet/cli <support@hexlet.io>
2024-08-06 21:59:09 +0300

    submit java-spring-ru/one-to-many

--- java-spring-ru/one-to-many/src/main/java/exercise/mapper/TaskMapper.java
+++ java-spring-ru/one-to-many/src/main/java/exercise/mapper/TaskMapper.java
@@ -4,12 +4,10 @@ import exercise.dto.TaskCreateDTO;
 import exercise.dto.TaskDTO;
 import exercise.dto.TaskUpdateDTO;
 import exercise.model.Task;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import exercise.model.User;
+import exercise.repository.UserRepository;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
@@ -17,11 +15,24 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class TaskMapper {
+    @Autowired
+    private UserRepository userRepository;
 
     @Mapping(target = "assignee.id", source = "assigneeId")
     public abstract Task map(TaskCreateDTO dto);
     @Mapping(source = "assignee.id", target = "assigneeId")
     public abstract TaskDTO map(Task model);
 
+
+    @Mapping(target= "assignee", source = "assigneeId", qualifiedByName = "getUserById")
     public abstract void update(TaskUpdateDTO dto, @MappingTarget Task model);
+
+    @Named("getUserById")
+    public User getUserById(Long id) {
+        if (id == null) {
+            return null;
+        } else {
+            return userRepository.findById(id).orElse(null);
+        }
+    }
 }
